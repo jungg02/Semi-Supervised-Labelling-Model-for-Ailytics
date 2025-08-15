7e2ea1d6146d11fb4ea8a84f7447ba8c208ddd4e
honjung <134697419+jungg02@users.noreply.github.com>
2025-08-15 14:46:35 +0800

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -1,3 +1,5 @@
+"" this file randomizes the dataset to prevent overfitting to certain conditions (lighting, angles, camera distances, backgrounds) and splits the data into training, val and test sets according to the specified split ratio. It also convverts the Labelme JSON files into YOLO text format to be used in model training ""
+
 import os
 import json
 import random
@@ -81,3 +83,4 @@ def prepare_dataset():
             convert_labelme_to_yolo(json_path, yolo_txt, labelme_classes, img_w, img_h)
 
 prepare_dataset()
+
